04391549c1bb35fff8d4189502fef942ba7112ac
Gonzalo Matheu (segmentation vm) <gmatheu@simtlix.com>
2023-06-26 16:23:38 +0000

    Adding install instructions

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # vim-minimal
+
+Bare minimum vim setup, targetting most of the useful features when accesing a remote server
+
+## Setup
+
+```
+curl -sL https://raw.githubusercontent.com/gmatheu/vim-minimal/master/install.sh | sh
+```
---
+++ install.sh
@@ -0,0 +1,38 @@
+#! /bin/bash
+
+VIM_CONFIG_HOME=$HOME/.config/vim-minimal
+
+NC=''
+error() {
+  color='[E] '
+  echo "${color}$1${NC} "
+}
+info() {
+  color='[I] '
+  echo "${color}$1${NC} "
+}
+check_bin() {
+  local bin=$1
+  which $bin >/dev/null 2>&1 && \
+    echo "true" || {
+    error "$bin is not available"
+    echo "false"
+  }
+}
+
+
+install() {
+    local exists=$(check_bin $bin)
+    if [ "$exists" = "true" ]; then
+      if [ ! -d "$VIM_CONFIG_HOME" ];
+      then
+        git clone https://github.com/gmatheu/vim-minimal.git "$VIM_CONFIG_HOME" || {
+        echo 'Could not clone repository'
+        exit 1
+      }
+      fi
+    fi
+    cd $VIM_CONFIG_HOME && ./bootstrap.sh
+}
+
+install
